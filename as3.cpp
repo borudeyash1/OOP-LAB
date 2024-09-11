debe3a2fbfb21cd184c3462f677c7477efcc9e86
borudeyash1 <143342463+borudeyash1@users.noreply.github.com>
2024-09-11 09:38:43 +0530

    as3.cpp

--- as3.cpp
+++ as3.cpp
@@ -1,27 +1,17 @@
-//developing an oop in in c++ to create  a database of personal information system 
-//containing the following information
-//name,dob,blood group,height,weight,insurance no.,contact no.,telephone no.,driving licence no.
-//construct the database with suitabble member functions
-//for initializing and destroying the data using constructor,default constructor,parameterized constructor,copy constructor
-//destructor,static data member and member fuction,friend function,inline finction and dynamic memory allocation operation new and delete 
-
 #include <iostream>
 #include <cstring>
 using namespace std;
+
 int n;
 
-class PersonalInfo
-{
+class PersonalInfo {
 private:
-    char name[100], dob[15],bg[5],insNo[20],contactNo[15],tn[15],dlNo[20];      
-    float height,weight;
+    char name[100], dob[15], bg[5], insNo[20], contactNo[15], tn[15], dlNo[20];
+    float height, weight;
     static int count;
-    
 
 public:
-
-    PersonalInfo()                         // Default constructor
-    {
+    PersonalInfo() {                         // Default constructor
         strcpy(name, "");
         strcpy(dob, "");
         strcpy(bg, "");
@@ -35,8 +25,7 @@ public:
     }
 
     // Parameterized constructor
-    PersonalInfo(const char *n, const char *d, const char *b, float h, float w, const char *i, const char *c, const char *t, const char *dl)
-    {
+    PersonalInfo(const char *n, const char *d, const char *b, float h, float w, const char *i, const char *c, const char *t, const char *dl) {
         strcpy(name, n);
         strcpy(dob, d);
         strcpy(bg, b);
@@ -50,8 +39,7 @@ public:
     }
 
     // Copy constructor
-    PersonalInfo(const PersonalInfo &p)
-    {
+    PersonalInfo(const PersonalInfo &p) {
         strcpy(name, p.name);
         strcpy(dob, p.dob);
         strcpy(bg, p.bg);
@@ -65,28 +53,32 @@ public:
     }
 
     // Destructor
-    ~PersonalInfo()
-    {
+    ~PersonalInfo() {
         count--;
     }
 
-    static int getCount()
-    {
+    static int getCount() {
         return count;
     }
-	
-    friend void display(PersonalInfo[],int);
-    void setHeightWeight(float h, float w)
-    {
+
+    // Friend functions
+    friend void display(PersonalInfo[], int);
+    friend void updateProfile(PersonalInfo[], int, const char*);
+    friend void deleteProfile(PersonalInfo[], int, const char*);
+
+    // Function to set height and weight
+    void setHeightWeight(float h, float w) {
         height = h;
         weight = w;
     }
-    inline void setWeight(float w) { weight = w; }
-    
+
+    // Inline function to set weight
+    inline void setWeight(float w) {
+        weight = w;
+    }
 
     // Function to take input from user
-    void input()
-    {
+    void input() {
         cout << "Enter name: ";
         cin >> name;
 
@@ -114,56 +106,115 @@ public:
         cout << "Enter driving license number: ";
         cin >> dlNo;
     }
-}y[10];
+
+    // Function to display personal information
+    void displayInfo() const {
+        cout << name << "\t" << dob << "\t" << bg << "\t" << height << "\t" << weight << "\t"
+             << insNo << "\t" << contactNo << "\t" << tn << "\t" << dlNo << endl;
+    }
+
+    // Function to compare insurance number
+    bool compareInsNo(const char *i) const {
+        return strcmp(insNo, i) == 0;
+    }
+};
 
 // Initialize static member
 int PersonalInfo::count = 0;
-        cout << "------------------------------------------Category Information---------------------------------------------------\n";
-void display(PersonalInfo p[],int i){
 
-    cout<<"\n";
-    for(i=0;i<n;i++)
-    cout<<p[i].name<<"\t"<<p[i].dob<<"\t"<<p[i].bg<<"\t"<<p[i].height<<"\t"<<p[i].weight<<"\t"<<p[i].insNo<<"\t"<<p[i].contactNo<<"\t"<<p[i].tn<<"\t"<<p[i].dlNo<<"\t"<<endl;}
-int main()
-{
-    PersonalInfo person;
-    int choice,i;
-    cout<<"Enter the number of records";
-    cin>>n;
+// Function to display personal information
+void display(PersonalInfo p[], int n) {
+    cout << "------------------------------------------Category Information---------------------------------------------------\n";
+    cout << "Name\tDOB\tBlood Group\tHeight\tWeight\tInsurance No\tContact No\tTelephone No\tDriving License No\n";
+    for (int i = 0; i < n; i++) {
+        p[i].displayInfo();
+    }
+}
 
-    while (choice != 4)
-    {
+// Function to update a profile
+void updateProfile(PersonalInfo p[], int n, const char *insNo) {
+    for (int i = 0; i < n; i++) {
+        if (p[i].compareInsNo(insNo)) {
+            cout << "Updating profile with Insurance Number " << insNo << endl;
+            p[i].input(); // Take new input for updating the profile
+            return;
+        }
+    }
+    cout << "Profile with Insurance Number " << insNo << " not found." << endl;
+}
+
+// Function to delete a profile
+void deleteProfile(PersonalInfo p[], int &n, const char *insNo) {
+    int i;
+    for (i = 0; i < n; i++) {
+        if (p[i].compareInsNo(insNo)) {
+            break;
+        }
+    }
+
+    if (i == n) {
+        cout << "Profile with Insurance Number " << insNo << " not found." << endl;
+        return;
+    }
+
+    for (int j = i; j < n - 1; j++) {
+        p[j] = p[j + 1];
+    }
+    n--;
+    cout << "Profile with Insurance Number " << insNo << " deleted." << endl;
+}
+
+int main() {
+    PersonalInfo person[10];
+    int choice = 0, i;
+    char insNo[20];
+
+    cout << "Enter the number of records: ";
+    cin >> n;
+
+    while (choice != 4) {
         cout << "\n--- Personal Information System ---\n";
         cout << "1. Add Personal Information\n";
         cout << "2. Display Personal Information\n";
-        cout << "3. Exit\n";
+        cout << "3. Update Profile\n";
+        cout << "4. Delete Profile\n";
+        cout << "5. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice)
-        {
-        case 1:
-        for(i=0;i<n;i++)
-        	{
-        	y[i].input();
-        	}
-            
-            break;
-        case 2:
-        	cout << "Name\t"<<"DOB\t"<<"Blood Group\t"<<"Height\t"<<"Weight\t"<<"Insurance No\t"<<"Contact No\t"<<"Telephone No\t"<<"Driving LicenseNo\t"<<endl;
-           display(y,i);
-    		
-            
-            break;
-        case 3:
-            cout << "Exiting the program.\n";
-            break;
-        default:
-            cout << "Invalid choice! Please try again.\n";
+        switch (choice) {
+            case 1:
+                for (i = 0; i < n; i++) {
+                    person[i].input(); // Take input for each person
+                }
+                break;
+
+            case 2:
+                display(person, n); // Display all persons' information
+                break;
+
+            case 3:
+                cout << "Enter Insurance Number to update: ";
+                cin >> insNo;
+                updateProfile(person, n, insNo); // Update profile
+                break;
+
+            case 4:
+                cout << "Enter Insurance Number to delete: ";
+                cin >> insNo;
+                deleteProfile(person, n, insNo); // Delete profile
+                break;
+
+            case 5:
+                cout << "Exiting the program.\n";
+                break;
+
+            default:
+                cout << "Invalid choice! Please try again.\n";
         }
-    };
+    }
 
     cout << "Total number of persons: " << PersonalInfo::getCount() << endl;
 
     return 0;
-}   
+}
